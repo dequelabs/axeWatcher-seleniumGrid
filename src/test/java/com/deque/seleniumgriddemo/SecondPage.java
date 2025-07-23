9a0aea2800be78e0967917f88e13c83c5f91559b
DQ_Padmavathi <dq_padmavathi@MacBookPro.Dlink>
2025-07-23 13:04:31 +0530

    selenium-grid-test

---
+++ src/test/java/com/deque/seleniumgriddemo/SecondPage.java
@@ -0,0 +1,36 @@
+package com.deque.seleniumgriddemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class SecondPage extends Base{
+
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        searchField.sendKeys("Selenium");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
+        submitButton.click();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        assertEquals(searchPageField.getAttribute("value"), "Selenium");
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.click();
+
+        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+        assertTrue(menuPopup.isDisplayed());
+    }
+
+
+}
